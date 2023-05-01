08e35b3090db1414f4061fd6368e8e6f21e80bf3
khushang bhavnagarwala <bkhushang@gmail.com>
2023-05-01 18:38:27 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,55 @@
+<?php
+
+// +------------------------------------------------------------------------+
+
+// | @author Deen Doughouz (DoughouzForest)
+
+// | @author_url 1: http://www.playtubescript.com
+
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+
+// | @author_email: wowondersocial@gmail.com   
+
+// +------------------------------------------------------------------------+
+
+// | PlayTube - The Ultimate Video Sharing Platform
+
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+
+// +------------------------------------------------------------------------+
+
+// MySQL Hostname
+
+$sql_db_host = "192.168.0.2";
+
+// MySQL Database User
+
+$sql_db_user = "laravel";
+
+// MySQL Database Password
+
+$sql_db_pass = "laravel";
+
+// MySQL Database Name
+
+$sql_db_name = "laravel_anywhereanycity_awac_tv";
+
+$sql_db_port = 3307;
+
+
+
+// Site URL
+
+// $site_url = "https://anywhereanycity.com/awactv"; // e.g (http://example.com)
+
+$site_url = "https://anywhereanycity.com/awactv"; // e.g (http://example.com)
+// $site_url = "http://127.0.0.1:8000/awactv"; // e.g (http://example.com)
+// $site_url_main = "http://127.0.0.1:8000"; // e.g (http://example.com)
+$site_url_main = "https://anywhereanycity.com/home"; // e.g (http://example.com)
+
+
+// Purchase code
+
+$purchase_code = "34976d70-e659-4de4-8ac8-425e886ed253"; // Your purchase code, don't give it to anyone. 
+
+?>
---
+++ config2.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "anywhere_wp";
+// MySQL Database Password
+$sql_db_pass = "u6p2psxmJtKI";
+// MySQL Database Name
+$sql_db_name = "anywhereanycity_staging_film";
+
+// Site URL
+$site_url = "https://staging.anywhereanycity.com/awactv"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "34976d70-e659-4de4-8ac8-425e886ed253"; // Your purchase code, don't give it to anyone. 
+?>
